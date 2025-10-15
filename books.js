14abcb1bb86ba901f383b2b52852c0599252fc2c
Philippe <philippe.idalgoprestes@gmail.com>
2025-10-15 07:38:38 -0300

    hum apostila teste

--- books.js
+++ books.js
@@ -80,5 +80,14 @@ export default
         pages:169,
         content:"Material completo para transformar suas aulas. Totalmente alinhada à BNCC e ao plano de curso da SEE-MG, este material oferece um estudo completo sobre a chegada dos europeus na América e as civilizações pré-colombianas. Com texto de apoio, atividades gabaritadas, e questões do ENEM comentadas, você terá em mãos a ferramenta completa para complementar suas aulas.",
         price:99.9
+    },
+    {
+        id:"p123",
+        src:"./assets/face/0.png",
+        title:"Apostila Teste",
+        author:"Philippe",
+        pages:196,
+        content:"Material completo para transformar suas aulas. Totalmente alinhada à BNCC e ao plano de curso da SEE-MG, este material oferece um estudo completo sobre a chegada dos europeus na América e as civilizações pré-colombianas. Com texto de apoio, atividades gabaritadas, e questões do ENEM comentadas, você terá em mãos a ferramenta completa para complementar suas aulas.",
+        price:13.4
     }
 ]
