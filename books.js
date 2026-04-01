da908bc46dd6aa706e5c38b8d088097923d1c1c5
Philippe <philippe.idalgoprestes@gmail.com>
2026-04-01 09:23:22 -0300

    imgs

--- books.js
+++ books.js
@@ -1,12 +1,57 @@
 export default
 [
-    {
-        id:"p123",
-        src:"./assets/face/0.png",
-        title:"Apostila Teste",
-        author:"Philippe",
-        pages:196,
-        description:"Material completo para transformar suas aulas. Totalmente alinhada à BNCC e ao plano de curso da SEE-MG, este material oferece um estudo completo sobre a chegada dos europeus na América e as civilizações pré-colombianas. Com texto de apoio, atividades gabaritadas, e questões do ENEM comentadas, você terá em mãos a ferramenta completa para complementar suas aulas.",
-        price:1.01
-    }
+  {
+    "id": "p124",
+    "src": "./assets/face/p124.png",
+    "title": "Produto Teste de c124",
+    "description": "Produto teste produto teste<br>Testando quebra de linha",
+    "author": "Criador Afiliado Teste Teste",
+    "pages": 123,
+    "price": 12.5
+  },
+  {
+    "id": "p123",
+    "src": "./assets/face/p123.png",
+    "title": "Produto Teste de c123 v2",
+    "description": "Produto teste produto teste3<br>Testando quebra de linha2",
+    "author": "Criador Teste Teste Asdei",
+    "pages": 123,
+    "price": 1.01
+  },
+  {
+    "id": "pbBf",
+    "src": "./assets/face/pbBf.png",
+    "title": "6º - História",
+    "description": "Material de história para o 6º ano. 345 páginas. ",
+    "author": "Paulo ",
+    "pages": 345,
+    "price": 99
+  },
+  {
+    "id": "pRWB",
+    "src": "./assets/face/pRWB.png",
+    "title": "7º - História",
+    "description": "Material de história para o 7º ano. 307 páginas. ",
+    "author": "Paulo ",
+    "pages": 307,
+    "price": 99
+  },
+  {
+    "id": "pRch",
+    "src": "./assets/face/pRch.png",
+    "title": "8º - História",
+    "description": "Material de história para o 8º ano. 259 páginas. ",
+    "author": "Paulo ",
+    "pages": 259,
+    "price": 99
+  },
+  {
+    "id": "pDwe",
+    "src": "./assets/face/pDwe.png",
+    "title": "9º - História",
+    "description": "Material de história para o 9º ano. 320 páginas. ",
+    "author": "Paulo ",
+    "pages": 320,
+    "price": 99
+  }
 ]
