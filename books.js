0b5b3a815dba5b01f8bb0e63da458e0c68502996
Philippe <philippe.idalgoprestes@gmail.com>
2026-05-26 22:42:18 -0300

    new books

--- books.js
+++ books.js
@@ -1,15 +1,6 @@
 export default
 [
   {
-    "id": "p124",
-    "src": "./assets/face/p124.png",
-    "title": "Produto Teste de c124",
-    "description": "Produto teste produto teste<br>Testando quebra de linha",
-    "author": "Criador Afiliado Teste Teste",
-    "pages": 123,
-    "price": 12.5
-  },
-  {
     "id": "p123",
     "src": "./assets/face/p123.png",
     "title": "Produto Teste de c123 v2",
@@ -19,39 +10,75 @@ export default
     "price": 1.01
   },
   {
+    "id": "p124",
+    "src": "./assets/face/p124.png",
+    "title": "Produto Teste de c124",
+    "description": "Produto teste produto teste<br>Testando quebra de linha",
+    "author": "Criador Afiliado Teste Teste",
+    "pages": 123,
+    "price": 12.5
+  },
+  {
+    "id": "p3cd",
+    "src": "./assets/face/p3cd.png",
+    "title": "32 temas de sociologia  - Sociologia",
+    "description": "âž¡ï¸ 32 temas de sociologia com cabeÃ§alho, texto de apoio e questÃµes ",
+    "author": "Paulo ",
+    "pages": 90,
+    "price": 59
+  },
+  {
     "id": "pbBf",
     "src": "./assets/face/pbBf.png",
-    "title": "6º - História",
+    "title": "6º Ano - História",
     "description": "Material de história para o 6º ano. 345 páginas. ",
     "author": "Paulo ",
     "pages": 345,
     "price": 99
   },
   {
-    "id": "pRWB",
-    "src": "./assets/face/pRWB.png",
-    "title": "7º - História",
-    "description": "Material de história para o 7º ano. 307 páginas. ",
+    "id": "pBut",
+    "src": "./assets/face/pBut.png",
+    "title": "148 temas de geografia - fundamental  - Geografia",
+    "description": "Mais de 100 atividades de geografia para o Ensino Fundamental com cabeçalho, texto de apoio e questões ",
     "author": "Paulo ",
-    "pages": 307,
+    "pages": 348,
+    "price": 99
+  },
+  {
+    "id": "pDwe",
+    "src": "./assets/face/pDwe.png",
+    "title": "9º ano - História",
+    "description": "Material de história para o 9º ano. 320 páginas. ",
+    "author": "Paulo ",
+    "pages": 320,
+    "price": 99
+  },
+  {
+    "id": "pQbB",
+    "src": "./assets/face/pQbB.png",
+    "title": "100 temas de Sociologia  - Sociologia",
+    "description": "100 temas de sociologia com cabeçalho, texto de apoio e questões ",
+    "author": "Paulo ",
+    "pages": 303,
     "price": 99
   },
   {
     "id": "pRch",
     "src": "./assets/face/pRch.png",
-    "title": "8º - História",
+    "title": "8º ano - História",
     "description": "Material de história para o 8º ano. 259 páginas. ",
     "author": "Paulo ",
     "pages": 259,
     "price": 99
   },
   {
-    "id": "pDwe",
-    "src": "./assets/face/pDwe.png",
-    "title": "9º - História",
-    "description": "Material de história para o 9º ano. 320 páginas. ",
+    "id": "pRWB",
+    "src": "./assets/face/pRWB.png",
+    "title": "7º ano - História",
+    "description": "Material de história para o 7º ano. 307 páginas. ",
     "author": "Paulo ",
-    "pages": 320,
+    "pages": 307,
     "price": 99
   }
 ]
